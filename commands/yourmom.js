cfebaf4f97712b128df85ff9ded01f2d714cc6b6
BluDood <bludood@bludood.com>
2021-12-26 22:14:12 +0100

    yo momma so phat she couldn't see this commit

---
+++ commands/yourmom.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('yourmom')
+		.setDescription("yo momma so phat she couldn't run this command"),
+	async execute(interaction) {
+        async function getJoke() {
+			const data = await fetch('https://api.yomomma.info')
+			return data.json()
+		}
+		var data = await getJoke()
+        const embed = {
+			color: "#0064FF",
+			title: data.joke,
+			footer: {
+				text: `Powered by api.yomomma.info`,
+			},
+		};
+		return interaction.reply({ embeds: [embed] });
+	},
+};
